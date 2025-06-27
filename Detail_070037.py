ec5df325f59ad331ad3b727d49c0f2fd8596985b
Govardhan G S <110452098+Govar12dhan@users.noreply.github.com>
2025-06-27 19:01:48 +0530

    Add files via upload

---
+++ Detail_070037.py
@@ -0,0 +1,10 @@
+Id = int(input("Enter your Student Id:"))     # User Input of student Id
+Name = input("Enter your student Name:")      # User Input of student name
+age = int(input("Enter your student Age:"))   # User Input of student age
+city = input("Enter your City:")              # User Input of student city
+phone_number = int(input("Enter your Phone number:"))   # User Input of student phone number
+print("Student Id:", Id)                      # output of Student Id
+print("Student Name is:", Name)               # output of Student name
+print("Student Age is:", age)                 # output of Student age
+print("Student City is:", city)               # output of Student city
+print("Student Phone Number:", phone_number)  # output of Student phone number
